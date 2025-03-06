48a31dd2a0230de8abbf2974313eb8cefeefc6ef
DavidJJ3715 <davidjj3715@gmail.com>
2025-03-05 20:39:49 -0500

    Start and pause menu implemented, can now either work on art for start screen or work on game features

--- func.h
+++ func.h
@@ -23,4 +23,158 @@
 #include "../SDL2/include/SDL2/SDL_ttf.h"
 #include "../SDL2/include/SDL2/SDL_image.h"
 
+/********************************
+*       Global Variables        *
+*********************************/
+const int WIDTH = 1200, HEIGHT = 800, frameDelay = 10;
+
+/********************************
+*       Core Functionality      *
+*********************************/
+
+/********************************
+*       Draw Functions          *
+*********************************/
+void drawStart(SDL_Renderer* renderer, TTF_Font* font, bool selection) {
+    // Placeholder code so I have a start menu
+    SDL_SetRenderDrawColor(renderer,255,255,255,0);
+    SDL_RenderClear(renderer);
+
+    int menuWidth = (WIDTH/2), menuHeight = 2*(HEIGHT/3); //Bounds of the black box
+    int menuX = (WIDTH - menuWidth)/2, menuY = (HEIGHT - menuHeight)/2; //Starting points of the black box
+    int widthVal = (WIDTH/2)-145, heightVal = -1;
+    SDL_Rect rect = {menuX, menuY, menuWidth, menuHeight}, border; //Black rectangle rect
+
+    SDL_SetRenderDrawColor(renderer,0,0,0,0); //Choose black color
+    SDL_RenderFillRect(renderer, &rect); //Fill in black rectangle
+
+    SDL_Surface* surface = TTF_RenderText_Solid(font, "Horde Explorer", {255,255,255,0}); //Game pause text
+    SDL_Texture* pauseText = SDL_CreateTextureFromSurface(renderer, surface); //Make it a texture
+    SDL_FreeSurface(surface); //Free the surface to prevent memory leaks
+    surface = TTF_RenderText_Solid(font, "PLAY", {255,255,255,0}); //Resume text
+    SDL_Texture* resumeText = SDL_CreateTextureFromSurface(renderer, surface); //Make it a texture
+    SDL_FreeSurface(surface); //Free the surface to prevent memory leaks
+    surface = TTF_RenderText_Solid(font, "QUIT", {255,255,255,0}); //Quit text
+    SDL_Texture* quitText = SDL_CreateTextureFromSurface(renderer, surface); //Make it a texture
+    SDL_FreeSurface(surface); //Free the surface to prevent memory leaks
+
+    SDL_Rect pause = {(WIDTH/2)-200, (HEIGHT/2)-(menuHeight/2)+25, 400, 140}; //Location of "GAME PAUSED"
+    SDL_Rect resume = {(WIDTH/2)-100, (HEIGHT/2)-50, 200, 100}; //Location of "RESUME"
+    SDL_Rect quit = {(WIDTH/2)-100, (HEIGHT/2)+100, 200, 100}; //Location of "QUIT"
+    SDL_RenderCopy(renderer, pauseText, nullptr, &pause); //Render "GAME PAUSED" to the screen
+    SDL_RenderCopy(renderer, resumeText, nullptr, &resume); //Render "RESUME" to the screen
+    SDL_RenderCopy(renderer, quitText, nullptr, &quit); //Render "QUIT" to the screen
+
+    if(selection) //Change where border is located based on which choice is selected
+        {heightVal = (HEIGHT/2)-50;} //Resume
+    else
+        {heightVal = (HEIGHT/2)+100;} //Quit
+
+    SDL_SetRenderDrawColor(renderer,212,175,55,0); //Gold color for border
+    for(int i=0; i<5; i++)
+    {
+        border = {widthVal+i, heightVal+i, 300-2*i, 100-2*i};
+        SDL_RenderDrawRect(renderer, &border); //Draw the golden rectangle
+    }
+
+    SDL_RenderPresent(renderer); //Draw everything to the screen
+    SDL_DestroyTexture(pauseText); //Destroy every texture to prevent memory leaks
+    SDL_DestroyTexture(resumeText);
+    SDL_DestroyTexture(quitText);   
+}
+
+void drawPause(SDL_Renderer* renderer, TTF_Font* font, bool selection) {
+    SDL_SetRenderDrawColor(renderer,255,255,255,0);
+    SDL_RenderClear(renderer);
+
+    int menuWidth = (WIDTH/2), menuHeight = 2*(HEIGHT/3); //Bounds of the black box
+    int menuX = (WIDTH - menuWidth)/2, menuY = (HEIGHT - menuHeight)/2; //Starting points of the black box
+    int widthVal = (WIDTH/2)-145, heightVal = -1;
+    SDL_Rect rect = {menuX, menuY, menuWidth, menuHeight}, border; //Black rectangle rect
+
+    SDL_SetRenderDrawColor(renderer,0,0,0,0); //Choose black color
+    SDL_RenderFillRect(renderer, &rect); //Fill in black rectangle
+
+    SDL_Surface* surface = TTF_RenderText_Solid(font, "GAME PAUSED", {255,255,255,0}); //Game pause text
+    SDL_Texture* pauseText = SDL_CreateTextureFromSurface(renderer, surface); //Make it a texture
+    SDL_FreeSurface(surface); //Free the surface to prevent memory leaks
+    surface = TTF_RenderText_Solid(font, "RESUME", {255,255,255,0}); //Resume text
+    SDL_Texture* resumeText = SDL_CreateTextureFromSurface(renderer, surface); //Make it a texture
+    SDL_FreeSurface(surface); //Free the surface to prevent memory leaks
+    surface = TTF_RenderText_Solid(font, "QUIT", {255,255,255,0}); //Quit text
+    SDL_Texture* quitText = SDL_CreateTextureFromSurface(renderer, surface); //Make it a texture
+    SDL_FreeSurface(surface); //Free the surface to prevent memory leaks
+
+    SDL_Rect pause = {(WIDTH/2)-200, (HEIGHT/2)-(menuHeight/2)+25, 400, 140}; //Location of "GAME PAUSED"
+    SDL_Rect resume = {(WIDTH/2)-100, (HEIGHT/2)-50, 200, 100}; //Location of "RESUME"
+    SDL_Rect quit = {(WIDTH/2)-100, (HEIGHT/2)+100, 200, 100}; //Location of "QUIT"
+    SDL_RenderCopy(renderer, pauseText, nullptr, &pause); //Render "GAME PAUSED" to the screen
+    SDL_RenderCopy(renderer, resumeText, nullptr, &resume); //Render "RESUME" to the screen
+    SDL_RenderCopy(renderer, quitText, nullptr, &quit); //Render "QUIT" to the screen
+
+    if(selection) //Change where border is located based on which choice is selected
+        {heightVal = (HEIGHT/2)-50;} //Resume
+    else
+        {heightVal = (HEIGHT/2)+100;} //Quit
+
+    SDL_SetRenderDrawColor(renderer,212,175,55,0); //Gold color for border
+    for(int i=0; i<5; i++)
+    {
+        border = {widthVal+i, heightVal+i, 300-2*i, 100-2*i};
+        SDL_RenderDrawRect(renderer, &border); //Draw the golden rectangle
+    }
+
+    SDL_RenderPresent(renderer); //Draw everything to the screen
+    SDL_DestroyTexture(pauseText); //Destroy every texture to prevent memory leaks
+    SDL_DestroyTexture(resumeText);
+    SDL_DestroyTexture(quitText);
+}
+
+bool selectionMenu(SDL_Renderer* renderer, TTF_Font* font, std::function<void(SDL_Renderer*,TTF_Font*,bool)> func) {
+    bool selection = true;
+    func(renderer, font, selection);
+    while(true) {
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+            switch(event.type) {
+                case SDL_MOUSEBUTTONDOWN:
+                    return selection;
+                case SDL_QUIT:
+                    return selection;
+                case SDL_KEYDOWN:
+                    switch(event.key.keysym.sym) {
+                        case SDLK_ESCAPE:
+                            return true;
+                        case SDLK_RETURN:
+                            return selection;
+                        case SDLK_UP: //User selected "RESUME"
+                            selection = true;
+                            func(renderer, font, selection);
+                            break;
+                        case SDLK_DOWN: //User selected "QUIT"
+                            selection = false;
+                            func(renderer, font, selection);
+                            break;
+                    }
+                    break;
+                case SDL_MOUSEMOTION:
+                    int xPos = event.motion.x;
+                    int yPos = event.motion.y;
+                    if(xPos > (WIDTH/2)-100 && yPos < (WIDTH/2)+100) {
+                        if(yPos > (HEIGHT/2)-50 && yPos < (HEIGHT/2)+100) {
+                            selection = true;
+                        }
+                        else if(yPos > (HEIGHT/2)+100 && yPos < (HEIGHT/2)+200) {
+                            selection = false;
+                        }
+                        func(renderer, font, selection);
+                    }
+                    break;
+            }
+        }
+    }
+    return selection;
+}
+
+
 #endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,58 @@
 #include "func.h"
 
 int main() {
+    SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
+    IMG_Init(IMG_INIT_PNG);
+    
+    SDL_Window* window = SDL_CreateWindow("Horde Explorer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    TTF_Font* font = TTF_OpenFont("DejaVuSans.ttf", 75);
+
+    int frameTime = 0, xPos = WIDTH/2, yPos = HEIGHT/2;
+    Uint64 frameStart = 0;
+    bool running = true, beginning = true;
+
+    while(running) {
+        frameStart = SDL_GetTicks64();
+        if(beginning) {
+            running = selectionMenu(renderer, font, drawStart);
+            beginning = false;
+        }
+
+        SDL_Event event;
+        while(SDL_PollEvent(&event)) {
+            xPos = event.motion.x;
+            yPos = event.motion.y;
+            switch(event.type) {
+                case SDL_QUIT:
+                    running = false; break;
+                case SDL_KEYDOWN:
+                    if(event.key.keysym.sym == SDLK_ESCAPE) {
+                        if(!selectionMenu(renderer, font, drawPause)) {
+                            beginning = true; break;
+                        }
+                    }
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer,0,0,0,0);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+        frameTime = SDL_GetTicks64() - frameStart;
+        if(frameDelay > frameTime) {
+            SDL_Delay(frameDelay - frameTime);
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    TTF_CloseFont(font);
+    TTF_Quit();
+    SDL_Quit();
+
     return 0;
 }
